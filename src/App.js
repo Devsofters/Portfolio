f39b357973a7ff0f235f5831bc97c5fe59189ab6
Akash <devsoft623@gmail.com>
2026-08-17 20:38:54 +0530

    Animation

--- src/App.js
+++ src/App.js
@@ -5,26 +5,38 @@ import Skills from "./Pages/Skills"
 import Experience from "./Pages/Experience"
 import Contact from "./Pages/Contact"
 import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: false,
+            offset: 100,
+        });
+    }, []);
+
     return (
         <div className="app">
             <Navbar />
 
             <div className="content">
-                <div id="Home">
+                <div id="Home"  data-aos="fade-up">
                     <Home />
                 </div>
-                <div id="About">
+                <div id="About" data-aos="fade-up">
                     <About />
                 </div>
-                <div id="Skills">
+                <div id="Skills" data-aos="fade-up">
                     <Skills />
                 </div>
-                <div id="Experience">
+                <div id="Experience"  data-aos="fade-up">
                     <Experience />
                 </div>
-                <div id="Contact">
+                <div id="Contact" data-aos="fade-up">
                     <Contact />
                 </div>
             </div>
